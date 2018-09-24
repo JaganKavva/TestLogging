0b9c611e3e651106dcfbf6d141ae647eb3f426f2
JaganKavva <jagankavva@gmail.com>
2018-09-24 09:13:40 -0500

    Update seed.groovy

--- seed.groovy
+++ seed.groovy
@@ -14,15 +14,7 @@ job('test-job') {
             echo 'jagan trigger'
         }
     steps {
-        checkout scm
-	    step([
-		  $class: 'ExecuteDslScripts',
-		  scriptLocation: [targets: ['resources/jenkins/dsl/*.groovy', 'resources/jenkins/dsl/Releases/*.groovy'].join('\n')],
-		  removedJobAction: 'DISABLE',
-		  removedViewAction: 'DELETE',
-		  lookupStrategy: 'JENKINS_ROOT',
-		  additionalClasspath: 'resources/jenkins/dsl'
-	  ])
+        
             maven('-e clean test')
             echo 'jagan clean test'
         }
