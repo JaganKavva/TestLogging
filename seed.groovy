ab6275ee833a5c197c00f19effd33c870b7f69e2
JaganKavva <jagankavva@gmail.com>
2018-09-23 22:45:59 -0500

    Update seed.groovy

--- seed.groovy
+++ seed.groovy
@@ -4,14 +4,18 @@ job('test-job') {
     job('test-job') {
     options {
      buildDiscarder(logRotator(daysToKeepStr: '180'))
+        echo 'Jagan logrotator'
     }
     scm {
         git(gitUrl)
+        echo 'jagan giturl'
     }
     triggers {
         scm('*/15 * * * *')
+        echo 'jagan trigger'
     }
     steps {
         maven('-e clean test')
+        echo 'jagan clean test'
     }
 }
